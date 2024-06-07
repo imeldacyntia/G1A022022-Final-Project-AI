5f3381d4d282a9d42f68b00a08686e28f02aee62
imeldacyntia <124637094+imeldacyntia@users.noreply.github.com>
2024-06-07 19:35:17 +0700

    Add files via upload

---
+++ G1A022022- Frontend/Predictive Maintenance System/src/scripts/api.js
@@ -0,0 +1,17 @@
+// TODO: Silakan sesuaikan BASE URL dari endpoint Anda
+const ENDPOINT = {
+  predict: `${BASE_URL}/predict`,
+};
+
+class PredictAPI {
+  static async predict(data) {
+    const response = await fetch(ENDPOINT.predict, {
+      method: 'POST',
+      body: data,
+      redirect: 'follow',
+    });
+
+    const json = await response.json();
+    return json;
+  }
+}
